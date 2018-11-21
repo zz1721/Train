89987ff59de3e86efe81c87d8d7f19f1e7d673c7
Zhesi Zhuang <zhesizhuang@dhcp-10-105-47-139.wireless.northwestern.private>
2018-11-20 18:47:09 -0600

    added new

--- javascript/script.js
+++ javascript/script.js
@@ -9,13 +9,57 @@
 
 // }
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCbqukNwY_vmTc6WkUuCKvyy430sCwlxMk",
+    authDomain: "trainschedule-829f9.firebaseapp.com",
+    databaseURL: "https://trainschedule-829f9.firebaseio.com",
+    projectId: "trainschedule-829f9",
+    storageBucket: "trainschedule-829f9.appspot.com",
+    messagingSenderId: "335964255387"
+  };
+
+  firebase.initializeApp(config);
+
+  var dataRef = firebase.database();
+
+
+
+
 var toneFrequency = 10;
-var tsecondFrequency = 40;
-var tthreeFrequency = 20;
+var tsecondFrequency = 50;
+var tthreeFrequency = 25;
 
 var firstTrainTime= "5:30";
 var secondTrainTime= "4:00";
 var thirdTrainTime= "3:30";
+   // Capture Button Click
+//    $("#submitbt").on("click", function(event){
+
+//     event.preventDefault();
+
+//     alert("clicked!");
+//     // YOUR TASK!!!
+//     // Code in the logic for storing and retrieving the most recent user.
+//     // Don't forget to provide initial data to your Firebase database.
+
+
+//     name = $("#name-input").val().trim();
+//     destination = $("#Destination-input").val().trim();
+//     time = $("#Time-input").val().trim();
+//     Frequency = $("#Frequency-input").val().trim();
+
+//     // Code for the push
+//     dataRef.ref().push({
+    
+//       name: name,
+//       destination: destination,
+//       time: time,
+//       Frequency: Frequency,
+//       dateAdded: firebase.database.ServerValue.TIMESTAMP
+//     });
+//   });
+
 
 
  // First Time (pushed back 1 year to make sure it comes before current time)
@@ -82,4 +126,80 @@ var currentTime = moment();
     var nextthirdTrain = moment().add(tthirdMinutesTillTrain, "minutes");
     $(".thirdarrivalTime").html(nextthirdTrain)
     console.log("ARRIVAL TIME: " + moment(nextthirdTrain).format("hh:mm"));
+
+    
+    $("#add-train").on("click", function(event) {
+        event.preventDefault();
+      
+        // Grabs user input
+        var TrainName = $("#name-input").val().trim();
+        var Destination = $("#Destination-input").val().trim();
+        var Timeinput = moment($("#Time-input").val().trim(), "HH:mm").format("X");
+        var FrequencyInput = $("#Frequency-input").val().trim();
+      
+        // Creates local "temporary" object for holding employee data
+        var newEmp = {
+          name: TrainName,
+          Destination: Destination,
+          Timeinput: Timeinput,
+          FrequencyInput: FrequencyInput
+        };
+      
+        // Uploads employee data to the database
+        database.ref().push(newEmp);
+      
+        // Logs everything to console
+        console.log(newEmp.name);
+        console.log(newEmp.Destination);
+        console.log(newEmp.Timeinput);
+        console.log(newEmp.FrequencyInput);
+      
+        alert("Employee successfully added");
+      
+        // Clears all of the text-boxes
+        $("#name-input").val("");
+        $("#Destination-input").val("");
+        $("#Time-input").val("");
+        $("#Frequency-input").val("");
+      });
+      
+      // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+      database.ref().on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val());
+      
+        // Store everything into a variable.
+        var TrainName = childSnapshot.val().TraiNname;
+        var Destination = childSnapshot.val().Destination;
+        var Timeinput = childSnapshot.val().Timeinput;
+        var FrequencyInput = childSnapshot.val().FrequencyInput;
+      
+        // Employee Info
+        console.log(TrainName);
+        console.log(Destination);
+        console.log(Timeinput);
+        console.log(FrequencyInput);
+      
+        // Prettify the employee start
+        var Timeinputtt = moment.unix(Timeinput).format("HH:mm");
+      
+        // Calculate the months worked using hardcore math
+        // To calculate the months worked
+        var TimeMin = moment().diff(moment(Timeinput, "X"), "minutes");
+        console.log(TimeMin);
+      
+        // Calculate the total billed rate
+        // var empBilled = empMonths * empRate;
+        // console.log(empBilled);
+      
+        // Create the new row
+        var newRow = $("<tr>").append(
+          $("<td>").text(TrainName),
+          $("<td>").text(Destination),
+          $("<td>").text(Timeinput),
+          $("<td>").text(TimeMin),
+        );
+      
+        // Append the new row to the table
+        $("#hello > tbody").append(newRow);
+      });
 
